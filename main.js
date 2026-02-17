ba2dd1527502833236eab352eb696642b1a90205
sangjinbak <sangjinbak@gmail.com>
2026-02-17 13:45:35 +0000

    feat: 야식 메뉴 추천에 이미지 추가 기능 구현
    - 각 야식 메뉴에 해당하는 이미지를 함께 표시하도록 기능을 업데이트함
    - '나노바나나'와 '콘푸르스트'를 메뉴에 추가함
    - JavaScript 데이터 구조를 객체 배열로 변경하여 이름과 이미지 URL을 함께 관리
    - CSS를 수정하여 이미지가 포함된 카드 UI를 새롭게 디자인함

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const snacks = [
-    '치킨', '피자', '족발', '보쌈', '떡볶이',
-    '라면', '김치전', '파전', '곱창', '닭발',
-    '아이스크림', '마른안주', '과일', '햄버거', '만두'
+    { name: '치킨', image: 'https://i.imgur.com/gJT3pDc.png' },
+    { name: '피자', image: 'https://i.imgur.com/eT8sC8d.png' },
+    { name: '족발', image: 'https://i.imgur.com/39S4a2j.png' },
+    { name: '보쌈', image: 'https://i.imgur.com/MYyYx3h.png' },
+    { name: '떡볶이', image: 'https://i.imgur.com/i9B0YpL.png' },
+    { name: '라면', image: 'https://i.imgur.com/ANeO9p9.png' },
+    { name: '김치전', image: 'https://i.imgur.com/xThsL1M.png' },
+    { name: '파전', image: 'https://i.imgur.com/PzXAt7Z.png' },
+    { name: '곱창', image: 'https://i.imgur.com/pGCSv2Z.png' },
+    { name: '닭발', image: 'https://i.imgur.com/rQWjC2I.png' },
+    { name: '아이스크림', image: 'https://i.imgur.com/NGRS11f.png' },
+    { name: '마른안주', image: 'https://i.imgur.com/s60YwW1.png' },
+    { name: '과일', image: 'https://i.imgur.com/T0T9b1d.png' },
+    { name: '햄버거', image: 'https://i.imgur.com/8zS6wzI.png' },
+    { name: '만두', image: 'https://i.imgur.com/Ggqmr1n.png' },
+    { name: '나노바나나', image: 'https://i.imgur.com/yv6vjCV.png' },
+    { name: '콘푸르스트', image: 'https://i.imgur.com/v2z2uT0.png' }
 ];
 
 const generateBtn = document.getElementById('generate-btn');
@@ -21,5 +35,8 @@ generateBtn.addEventListener('click', () => {
 
     // Set new animation and content
     recommendationCard.style.animation = 'flip-in 0.6s ease-out forwards';
-    recommendationCard.textContent = selectedSnack;
+    recommendationCard.innerHTML = `
+        <img src="${selectedSnack.image}" alt="${selectedSnack.name}">
+        <div class="snack-name">${selectedSnack.name}</div>
+    `;
 });
